a13af164faef37dc30a9b0f9fb3ece8d0db57fe2
terappy <terappy@users.noreply.github.com>
2021-05-23 14:53:50 +0000

    deploy: 7e629b6aa19d1dc445e40f626ba637c5cf402ab3

---
+++ _next/static/TX9PTLy6G3xkp3pWS4uIv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-55324d770e732a2ac0d5.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],"/posts":["static/chunks/pages/posts-05c9fd5fc83ab487098b.js"],sortedPages:["/","/_app","/_error","/posts"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
